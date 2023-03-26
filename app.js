ba130e92860ce83d207b44204d6bcc75610cc8dd
IHOR <97735057+ihoretti@users.noreply.github.com>
2023-03-26 22:47:40 +0300

    summary(required)

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-require("dotenv").config();
 
-const authRouter = require("./routes/api/auth");
-
-const contactsRouter = require("./routes/api/contacts");
+const { authRouter, contactsRouter } = require("./routes/api");
 
 const app = express();
 
@@ -15,7 +12,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
+app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
@@ -23,12 +20,14 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  console.log(`App error: ${err.message}`);
+  console.log("App crashed!!!: ", err.stack);
+
   if (err.status) {
     return res.status(err.status).json({
       message: err.message,
     });
   }
+
   res.status(500).json({ message: "Internal server error" });
 });
 
