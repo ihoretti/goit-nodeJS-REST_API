7c8a6253aa9a825118cc937d785db183c2ae4a37
IHOR <97735057+ihoretti@users.noreply.github.com>
2023-03-29 00:26:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const config = {
   PORT: process.env.PORT || 3000,
   SERVER_HOST: process.env.SERVER_HOST || `http://localhost:3000`,
   DB_HOST: process.env.DB_HOST,
+
   SECRET_KEY: process.env.SECRET_KEY || "SECRET_KEY",
   UPLOAD_DIR_TMP: "tmp",
   UPLOAD_DIR_AVATARS: "public/avatars",
